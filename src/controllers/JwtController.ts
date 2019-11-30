6e1efa0eca082dddb2a02f124c5613988c559801
rizkyramadhan <rizky@andromedia.co.id>
2019-11-30 07:04:06 +0700

    fix

--- src/controllers/JwtController.ts
+++ src/controllers/JwtController.ts
@@ -4,11 +4,11 @@
  * created by Sean Maxwell Aug 26, 2018
  */
 
-import { OK, FORBIDDEN } from "http-status-codes";
-import { JwtManager, ISecureRequest } from "@overnightjs/jwt";
-import { Controller, Middleware, Get, Post, Put } from "@overnightjs/core";
+import { Controller, Get, Middleware, Post, Put } from "@overnightjs/core";
+import { ISecureRequest, JwtManager } from "@overnightjs/jwt";
 import { Request, Response } from "express";
-import config from "../config";
+import { FORBIDDEN, OK } from "http-status-codes";
+import Password from "node-php-password";
 const db = require("../db").default;
 
 export const jwtMgr = new JwtManager("12345678901234567890123456789012", "24h");
@@ -23,22 +23,23 @@ export class JwtController {
   @Post("login")
   private async login(req: Request, res: Response) {
     const users = await db.any(
-      "SELECT * FROM public.user WHERE username like $1 and password like $2",
-      [req.body.username, req.body.password]
+      "SELECT * FROM public.p_user WHERE username like $1 ",
+      [req.body.username]
     );
-    console.log(req.body)
     if (users.length > 0) {
-      const u = users[0];
-      const jwtStr = jwtMgr.jwt({
-        ...u,
-        time: new Date().toString(),
-        "https://hasura.io/jwt/claims": {
-          "x-hasura-allowed-roles": [u.role],
-          "x-hasura-default-role": u.role,
-          "x-hasura-user-id": u.id.toString()
-        }
-      });
-      return res.status(OK).send(jwtStr);
+      if (Password.verify(req.body.password, users[0].password)) {
+        const u = users[0];
+        const jwtStr = jwtMgr.jwt({
+          ...u,
+          time: new Date().toString(),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": [u.role],
+            "x-hasura-default-role": u.role,
+            "x-hasura-user-id": u.id.toString()
+          }
+        });
+        return res.status(OK).send(jwtStr);
+      }
     }
 
     return res.status(FORBIDDEN).json({
